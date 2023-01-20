ab00667b926594fbe3d16d6faebe02e2b0b168a2
Karol Binkowski <starystachu9@gmail.com>
2023-01-20 17:08:41 +0100

    278. First Bad Version

---
+++ solutions/278. First Bad Version/first-bad-version.py
@@ -0,0 +1,28 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        low = 0
+        mid = None
+        high = n
+        result = n
+
+        while low <= high:
+            mid = int((low + high) / 2)
+            is_bad = isBadVersion(mid)
+
+            if is_bad:
+                result = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return result
