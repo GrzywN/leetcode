9842846a0e31492ee56b957c960b89d6ee09c992
Karol Binkowski <karolbinkowski3@proton.me>
2023-04-25 14:57:31 +0200

    724. Find Pivot Index

---
+++ solutions/724. Find Pivot Index/find-pivot-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        total = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            right_sum = total - nums[i] - left_sum
+
+            if left_sum == right_sum:
+                return i
+
+            left_sum += nums[i]
+
+        return -1
