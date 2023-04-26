60d56c902aa03ba99ff74c2038b9e3953c58aed1
Karol Binkowski <starystachu9@gmail.com>
2023-04-26 11:01:53 +0200

    392. Is Subsequence

---
+++ solutions/392. Is Subsequence/is-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        left = 0
+        right = 0
+
+        while left < len(s) and right < len(t):
+            if s[left] == t[right]:
+                left += 1
+            right += 1
+
+        return left == len(s)
