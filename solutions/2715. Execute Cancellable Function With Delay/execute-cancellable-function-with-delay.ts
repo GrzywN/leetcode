f77353d09d2904d1e343fa8dd36630dd555d2de4
Karol Binkowski <starystachu9@gmail.com>
2023-06-06 11:47:09 +0200

    2715. Execute Cancellable Function With Delay

---
+++ solutions/2715. Execute Cancellable Function With Delay/execute-cancellable-function-with-delay.ts
@@ -0,0 +1,26 @@
+function cancellable(fn: Function, args: any[], t: number): Function {
+  const timeout = setTimeout(() => fn(...args), t)
+  const cancelFn = () => {
+    clearTimeout(timeout)
+  };
+
+  return cancelFn;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const log = (...argsArr) => {
+ *      result.push(fn(...argsArr))
+ *  }
+ *
+ *  const cancel = cancellable(fn, args, t);
+ *
+ *  setTimeout(() => {
+ *     cancel()
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, cancelT)
+ */
