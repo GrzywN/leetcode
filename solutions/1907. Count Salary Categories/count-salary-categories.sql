505ba16b7a4aec1f7f421c724a14c96b745c877f
Karol Binkowski <starystachu9@gmail.com>
2023-06-29 20:31:10 +0200

    1907. Count Salary Categories

---
+++ solutions/1907. Count Salary Categories/count-salary-categories.sql
@@ -0,0 +1,20 @@
+SELECT
+  "Low Salary" AS category,
+  SUM(income < 20000) AS accounts_count
+FROM
+  Accounts
+UNION
+SELECT
+  "Average Salary" AS category,
+  SUM(
+    income >= 20000
+    AND income <= 50000
+  ) AS accounts_count
+FROM
+  Accounts
+UNION
+SELECT
+  "High Salary" AS category,
+  SUM(income > 50000) AS accounts_count
+FROM
+  Accounts;
