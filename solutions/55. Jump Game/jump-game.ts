95474afaff54af7e199bdadf677b202dfa29262e
GrzywN <karolbinkowski3@proton.me>
2024-05-03 23:01:46 +0200

    55. Jump Game

---
+++ solutions/55. Jump Game/jump-game.ts
@@ -0,0 +1,11 @@
+function canJump(nums: number[]): boolean {
+  let goal = nums.length - 1;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= goal) {
+      goal = i;
+    }
+  }
+
+  return goal == 0;
+}
